1a8559eccf674d6247172394552e95ff20972bbd
amaraa <moonkeymon924@gmail.com>
2022-12-13 15:08:00 +0800

    modal overlay duusla

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,6 +8,25 @@ new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 
 let mobileMenu = new MobileMenu();
+
+var modal;
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/*webpackChunkName: 'modal'*/ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openModal(), 20);
+        })
+        .catch(() => alert("error"));
+    } else {
+      modal.openModal();
+    }
+  });
+});
+
 // if (module.hot) {
 //   module.hot.accept();
 // }
